a9b522efc9ad5d3e180a7c588f48ee11dea8b4d0
Carlo Field <carlo@swiss.dev>
2020-04-19 01:35:50 +0200

    add additional files

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/cfi2017/bl3-bot
+
+go 1.14
+
+require (
+	github.com/bwmarrin/discordgo v0.20.3
+	github.com/cfi2017/bl3-save v1.0.3
+	github.com/cfi2017/dgcobra v0.0.0-20200309102910-1d14dde1fca9
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/golang/protobuf v1.4.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mitchellh/mapstructure v1.2.2 // indirect
+	github.com/pelletier/go-toml v1.7.0 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.6.3 // indirect
+	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904 // indirect
+	golang.org/x/sys v0.0.0-20200413165638-669c56c373c4 // indirect
+	gopkg.in/ini.v1 v1.55.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/cfi2017/dgcobra"
+)
+
+var (
+	token   = flag.String("token", "", "token")
+	handler *dgcobra.Handler
+)
+
+func main() {
+	flag.Parse()
+	if *token == "" {
+		log.Fatal("missing token")
+	}
+
+	discord, err := discordgo.New("Bot " + *token)
+	if err != nil {
+		panic(err)
+	}
+
+	handler = dgcobra.NewHandler(discord)
+	handler.AddPrefix("!")
+	handler.RootFactory = rootFactory
+	handler.Start()
+
+	discord.AddHandlerOnce(onReady)
+
+	err = discord.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Bot is running. Press CTRL-C to exit.")
+	waitForSig()
+	err = discord.Close()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func onReady(_ *discordgo.Session, ready *discordgo.Ready) {
+	handler.AddPrefix(fmt.Sprintf("<@!%s> ", ready.User.ID))
+}
+
+func waitForSig() {
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
